4febd5b973b0ec5607ee25dc369d3f8a79140934
dhruvikadhiwala <dhruvik@terpmail.umd.edu>
2024-04-06 16:49:37 -0400

    Added a new class

--- project.py
+++ project.py
@@ -1,5 +1,17 @@
-print("hello")
-print("goodbye")
-print("hi")
-print("bye")
-print("hey")
+import datetime
+
+class Expense:
+    """Class to represent an expense
+    """
+    def __init__(self, amount, category, description):
+        """Initialize an expense object
+        
+        Args:
+            amount (float): the amount of the expense
+            category (str): the category of the expense
+            description (str): description of the expense
+        """
+        self.amount = amount
+        self.category = category
+        self.description = description
+        self.date = datetime.datetime.now()
