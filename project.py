41b3255e89060c827b11a7064b885541676a51c4
isabellemtho <ithomas6@terpmail.umd.edu>
2024-04-06 16:52:06 -0400

    Update project.py

--- project.py
+++ project.py
@@ -15,3 +15,23 @@ class Expense:
         self.category = category
         self.description = description
         self.date = datetime.datetime.now()
+        
+class BudgetApp:
+    """Class to represent a budgeting and expense tracking application."""
+
+    def __init__(self):
+        """Initialize a BudgetApp object."""
+        self.expenses = []  # List to store expense objects
+        self.monthly_budgets = {}  # Dictionary to store monthly budgets
+
+    def add_expense(self, amount, category, description):
+        """
+        Add an expense to the list of expenses.
+
+        Parameters:
+        - amount (float): The amount of the expense.
+        - category (str): The category of the expense.
+        - description (str): Description of the expense.
+        """
+        expense = Expense(amount, category, description)
+        self.expenses.append(expense)
